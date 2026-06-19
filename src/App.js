513b807a589bdd3735b6b6c6928d89dba1a211f6
BannyT <tumusiimebarnabus@gmail.com>
2026-06-19 12:55:25 +0300

    updates data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Carousel from './Carousel';
 
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <Carousel/>
     </div>
   );
 }
