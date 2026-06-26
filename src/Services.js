ca18211471adc261121da81f0c68d98030e29f3a
BannyT <tumusiimebarnabus@gmail.com>
2026-06-26 09:57:06 +0300

    updates code

--- src/Services.js
+++ src/Services.js
@@ -38,7 +38,7 @@ function Services() {
     <div className='services-section'>
        <div className='services-container'>
            <div className='services-header'>
-                <span className='servives-badge'>What we Offer</span>
+                <span className='services-badge'>What we Offer</span>
                 <h2 className='services-title'>Our Premium Services</h2>
                 <p className='services-subtitle'>Empowering  Your Future with industry-leading training and support</p>
            </div>
