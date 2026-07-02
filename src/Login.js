cf8cf1036fe5708d34ebe631eba29692e998dcdb
BannyT <tumusiimebarnabus@gmail.com>
2026-07-02 09:24:11 +0300

    updates css file for login

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./Login.css"
 
 function Login() {
   return (
-    <div>
-      <h2>This is Login</h2>
+    <div className='login-page'>
+          <div className='login-card'>
+            <div className='login-header'>
+               <span className='login-brand'>Stratcom</span>
+               <h2>Welcome Back</h2>
+               <p>Sign In to Continue ...</p>
+            </div>
+
+          </div>
     </div>
   )
 }
