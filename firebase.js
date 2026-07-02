ec1bdab49009d2954a692a473b6a336affcdb14a
BannyT <tumusiimebarnabus@gmail.com>
2026-07-02 12:58:11 +0300

    updates data

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDsetUCeMH_HVV-oKJ1ulMSKhmb3Q24YBg",
+    authDomain: "group2-da93d.firebaseapp.com",
+    projectId: "group2-da93d",
+    storageBucket: "group2-da93d.firebasestorage.app",
+    messagingSenderId: "974956319159",
+    appId: "1:974956319159:web:8f34f28a0719b946afd832",
+    measurementId: "G-WYWTBXYZR9"
+  };
