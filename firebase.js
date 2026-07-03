21e645c89a9f1aa240f03b44ee2aa118e57c7edf
BannyT <tumusiimebarnabus@gmail.com>
2026-07-03 09:14:26 +0300

    updates data

--- firebase.js
+++ firebase.js
@@ -1,3 +1,9 @@
+import {initializeApp} from 'firebase/app'
+import {getAuth,signInWithEmailAndPassword,createUserWithEmailAndPassword,signOut,updateProfile} from 'firebase/auth'
+import {getFireStore,collection,addDoc,getDocs,updateDoc,deleteDoc,doc,onSnapShot,query,orderBy} from 'firebase/firestore'
+
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyDsetUCeMH_HVV-oKJ1ulMSKhmb3Q24YBg",
     authDomain: "group2-da93d.firebaseapp.com",
@@ -7,3 +13,12 @@ const firebaseConfig = {
     appId: "1:974956319159:web:8f34f28a0719b946afd832",
     measurementId: "G-WYWTBXYZR9"
   };
+
+  //initialzing firebase app here
+
+  const app = initializeApp(firebaseConfig)
+  const auth = getAuth(app)
+  const db = getFireStore(app)
+
+
+export{auth,db,signInWithEmailAndPassword,createUserWithEmailAndPassword,updateProfile,updateDoc,collection,deleteDoc,doc,onSnapShot,query,orderBy,doc,getDocs};
