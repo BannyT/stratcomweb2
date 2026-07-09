0bc514bf3023134638707e18847230740e058ae0
BannyT <tumusiimebarnabus@gmail.com>
2026-07-09 10:47:40 +0300

    updates dada

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -17,6 +17,38 @@ function Dashboard() {
                  <h1 className='high-light'>My Activities</h1>
                  <p>Track Your Intership Activities</p>
            </div>
+           <div className='form-card'>
+                 <h3> Add New Activity</h3>
+                 <form>
+                     <div className='form-group'>
+                         <label htmlFor='title'> Title</label>
+                         <input 
+                             type='text'
+                             placeholder='e.g Configuring Router'
+                           />
+                     </div>
+                     <div className='form-group'>
+                         <label htmlFor='subtitle'> subtitle</label>
+                         <input 
+                             type='text'
+                             placeholder='e.g Configuring Router'
+                           />
+                     </div>
+
+                     <div className='form-group'>
+                         <label htmlFor='description'> Description</label>
+                         <textarea
+                            rows='3' 
+                             type='text'
+                             placeholder='e.g Configuring Router'
+                           />
+                     </div>
+                     <div className='form-actions'>
+                         <button className='btn-primary'>Add Activity</button>
+                         <button className='btn-secondary'>Cancel</button>
+                     </div>
+                 </form>
+           </div>
 
        </div>
        
